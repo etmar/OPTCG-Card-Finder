bb5df9365327cf063c9bfe7f2f9f854513fcf076
Marc-Olivier Etienne <43760193+etmar@users.noreply.github.com>
2023-08-06 03:02:12 -0400

    comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import discord
 cards = json.load(open("cardinventory.json"))
 # TODO: handle missing 'bot-token.txt'
 token = open('bot-token.txt').read()
+# TODO: handle empty bot-token file
 
 
 def card_search_name(card_name):
